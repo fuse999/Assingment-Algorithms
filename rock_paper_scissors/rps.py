076719d7477e8a2f15211ed3c2ed8e7d120ffb1b
riley pence <rileypence4@gmail.com>
2020-03-19 13:09:26 -0700

    Update rps.py

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -2,8 +2,26 @@
 
 import sys
 
+
 def rock_paper_scissors(n):
-  pass 
+  possible_plays = [["rock"], ["paper"], ["scissors"]]
+  if n == 0:
+    return [[]]
+
+  if n == 1:
+    return possible_plays
+
+  multi_plays = []
+  rounds = rock_paper_scissors(n - 1)
+
+  for round in rounds:
+
+     for play in possible_plays:
+
+       new_play = round + play
+       multi_plays.append(new_play)
+
+  return multi_plays
 
 
 if __name__ == "__main__":
