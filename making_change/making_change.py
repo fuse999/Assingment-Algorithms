59c0a67a4dafe89381de9f8412e502d1bdd25d62
riley pence <rileypence4@gmail.com>
2020-03-19 13:51:32 -0700

    genral updates

--- making_change/making_change.py
+++ making_change/making_change.py
@@ -2,12 +2,20 @@
 
 import sys
 
+
 def making_change(amount, denominations):
+    # Create an array of zeros to track number of ways to make change
     ways = [0 for i in range(amount + 1)]
+    # Initialize ways[0] as 1 as base case
     ways[0] = 1
+    # For each denomination:
     for denom in denominations:
+        # Create a range from 1 to amount + 1
         for amt in range(1, amount + 1):
+            # Confirm the denomination is less than or equal to
+            # the current amt
             if denom <= amt:
+                # Increment at that amt position in the ways array
                 ways[amt] += ways[amt - denom]
     return ways[amount]
 
